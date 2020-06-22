66d6aa2b9a5ff644b8f3fd152213fedba5770fa4
Uzair Shahid <uzairshahid7111@gmail.com>
2020-06-22 15:06:13 +0500

    about updates

---
+++ js/allAcceptHome.js
@@ -0,0 +1,12 @@
+ // Navbar transparent
+ $(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 0.1) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
